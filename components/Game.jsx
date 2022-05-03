2de730a0bd037da29a93e86c04995296b31057c1
Lorenzo Lizano Alpizar <lorenzolizanocr@gmail.com>
2022-05-02 19:45:51 -0600

    class 5

--- components/Game.jsx
+++ components/Game.jsx
@@ -1,22 +1,42 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Number from './Number';
 
 
-export default Game = ({ randomNumbers }) => { 
+export default Game = ({ randomNumbersCount }) => { 
   
- // const target = 10 + Math.floor(40 * Math.random());
+const[randomNumbers, setRandomNumbers]  = useState ([]);
+const[target, setTarget] = useState ([]);
+const[selectedNumbers, setSelectedNumbers] = useState ([]);
 
-  const numbers = Array.from({ length : randomNumbers}).map(() => 1 + Math.floor(10 * Math.random()));
 
-  const target = numbers.slice(0, randomNumbers -2).reduce( (acc, cur) => acc + cur,0);
+useEffect(() => {
+  const numbers = Array.from({ length : randomNumbersCount}).map(() => 1 + Math.floor(10 * Math.random()));
+  const target = numbers.slice(0, randomNumbersCount -2).reduce( (acc, cur) => acc + cur,0);
+  
+  setRandomNumbers(numbers);
+  setTarget(target);
+  
+}, []);
+  
+
+  const isNumberSelected = numberIndex => selectedNumbers.some(number => number === numberIndex);
+
+  const selectNumber = number => setSelectedNumbers([...selectedNumbers, number]);
   
   
   return (
     <View>
         <Text style={styles.target}>{target}</Text>
         <View style={styles.randomContainer}>
-        {numbers.map((number,index) => (
-          <Text key={index} style = {styles.random}>{number}</Text>
+          {randomNumbers.map((number,index) => (
+           <Number 
+            key={index} 
+            id={index} 
+            number={number} 
+            isSelected ={isNumberSelected(index)} 
+            onSelected={selectNumber} 
+          />
         ))}
         </View>
     </View>
@@ -38,13 +58,5 @@ const styles = StyleSheet.create({
       flexWrap: 'wrap',
       justifyContent: 'space-between',
     },
-    random: {
-      backgroundColor: '#999',
-      width: 100,
-      marginHorizontal: 15,
-      marginVertical: 15,
-      fontSize : 35,
-      textAlign: 'center',
-    }
   });
   
